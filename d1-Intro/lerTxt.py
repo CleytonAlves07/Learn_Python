b7acb046897150320a4168c166d83da4aa1c3aa0
Cleyton Alves <cleyton.alves.a@gmail.com>
2023-01-16 14:34:22 -0300

    feat: :zap: Manipulando arquivo .txt com python
    Escrevendo ( escreve por cima) ou anexando (adiciona no arquivo) em .txt

--- d1-Intro/lerTxt.py
+++ d1-Intro/lerTxt.py
@@ -1,7 +1,17 @@
 file = 'pythontxt.txt'
 
+# Para escrever - escreve por cima do conteúdo que já existir
+with open(file, 'w') as file_object:
+  file_object.write('Se você está lendo isso, é melhor voltar para o último commit')
+
+# Para anexar algo
+with open(file, 'a') as file_object:
+  file_object.write('Espero aprender mais sobre automação, Machine Learn e IA')
+
+# Para ler 
 with open(file) as file_object:
   lines = file_object.readlines()
 
 for line in lines:
   print(line)
+
