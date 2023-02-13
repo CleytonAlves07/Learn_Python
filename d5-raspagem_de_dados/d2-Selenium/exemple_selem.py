058681e875a323dc021488f5eea3018e911f2d88
Cleyton Alves <cleyton.alves.a@gmail.com>
2023-02-13 20:03:11 -0300

    feat: Utilizando a common.by do webdriver para acessar o mesmo input de um forma diferente

--- d5-raspagem_de_dados/d2-Selenium/exemple_selem.py
+++ d5-raspagem_de_dados/d2-Selenium/exemple_selem.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 firefox = webdriver.Firefox()
 
 response = firefox.get("https://www.python.org/")
 
-search_input = firefox.find_element("name", "q")
-search_input.send_keys("selenium")
-search_input.send_keys(Keys.ENTER)
+# search_input = firefox.find_element("name", "q")
+# search_input.send_keys("selenium")
+# search_input.send_keys(Keys.ENTER)
+
+search_input = firefox.find_element(By.NAME, 'q')
+search_input.send_keys('selenium')
+search_input.submit()
