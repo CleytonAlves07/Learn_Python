6eb2a0964e966c035042c3f6c71c925b7f5a98fe
Cleyton Alves <cleyton.alves.a@gmail.com>
2023-02-13 20:15:16 -0300

    feat: Utilizando selenium para pegar título, preço e link da página book scrape

--- d5-raspagem_de_dados/d2-Selenium/exemple_selem.py
+++ d5-raspagem_de_dados/d2-Selenium/exemple_selem.py
@@ -4,12 +4,38 @@ from selenium.webdriver.common.by import By
 
 firefox = webdriver.Firefox()
 
-response = firefox.get("https://www.python.org/")
+# response = firefox.get("https://www.python.org/")
 
 # search_input = firefox.find_element("name", "q")
 # search_input.send_keys("selenium")
 # search_input.send_keys(Keys.ENTER)
 
-search_input = firefox.find_element(By.NAME, 'q')
-search_input.send_keys('selenium')
-search_input.submit()
+# search_input = firefox.find_element(By.NAME, 'q')
+# search_input.send_keys('selenium')
+# search_input.submit()
+
+
+def scrape(url):
+    firefox.get(url)
+
+    for book in firefox.find_elements(By.CLASS_NAME, "product_pod"):
+        new_item = {}
+        new_item["title"] = (
+            book.find_element(By.TAG_NAME, 'h3')
+            .find_element(By.TAG_NAME, 'a')
+            .get_attribute("innerHTML")
+        )
+        new_item["price"] = (
+            book.find_element(By.CLASS_NAME, 'price_color')
+            .get_attribute("innerHTML")
+        )
+        new_item["link"] = (
+            book.find_element(By.CLASS_NAME, "image_container")
+            .find_element(By.TAG_NAME, 'a')
+            .get_attribute('href')
+        )
+
+        print(new_item)
+
+
+scrape("https://books.toscrape.com/")
