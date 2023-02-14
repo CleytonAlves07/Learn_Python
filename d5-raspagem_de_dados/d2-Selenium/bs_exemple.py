5d083cfc500c14b20615babb1f3c9130aa381336
Cleyton Alves <cleyton.alves.a@gmail.com>
2023-02-14 09:40:11 -0300

    Raspagens iniciais com o soupBeautiful

--- d5-raspagem_de_dados/d2-Selenium/bs_exemple.py
+++ d5-raspagem_de_dados/d2-Selenium/bs_exemple.py
@@ -1,5 +1,31 @@
 import requests
+from bs4 import BeautifulSoup
+from rich import print
+
 
 url = "https://quotes.toscrape.com"
 page = requests.get(url)
-print(page.content)
+html_content = page.text
+soup = BeautifulSoup(html_content, 'html.parser')
+
+
+# print(soup.prettify())
+
+title = soup.title
+# print(title)
+
+# print(title.name)
+# print(title.string)
+# print(type(title.string))
+
+# footer = soup.footer
+
+# print(footer['class'])
+
+# print(soup.find_all('p'))
+
+# print(soup.find(id="quote"))
+
+# print(soup.get_text())
+
+print(soup.find_all('div', {'class', 'quote'}))
